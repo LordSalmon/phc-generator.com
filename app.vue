f37cbe6655ec73ad17503201a5e7e625bafe3708
Simon Schwedes <simon@schwedes.eu>
2023-05-18 13:42:05 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <span class="bg-red-500">test</span>
+        <NuxtWelcome />
+    </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+        [
+            "@nuxtjs/eslint-module",
+            {
+                /* module options */
+                lintOnStart: true,
+                emitError: true,
+                emitWarning: true,
+                failOnError: false,
+                failOnWarning: false,
+            },
+        ],
+        [
+            "@nuxtjs/tailwindcss",
+            {
+                configPath: "tailwind.config.js",
+                exposeConfig: true,
+                injectPosition: "first",
+                viewer: true,
+            },
+        ],
+        [
+            "@nuxtjs/plausible",
+            {
+                domain: "phc-generator.com",
+                apiHost: "https://plausible.schwedes.eu",
+                trackLocalhost: true,
+            },
+        ],
+    ],
+});
